a735f863e40df589f78d49b880182a271fc3c838
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-07-16 18:49:54 +0545

    error handling

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
-require('dotenv').config()
-
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler-middleware");
 
 //middleware
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 app.use(express.json());
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
